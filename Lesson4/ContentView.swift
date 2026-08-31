21633188a0244735378428d5b4edd5232e2a61d9
BlairBelcher <belcher.blair09@gmail.com>
2026-08-31 13:01:34 -0400

    Initial Commit

---
+++ Lesson4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson4
+//
+//  Created by user302134 on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson4/Lesson4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4App.swift
+//  Lesson4
+//
+//  Created by user302134 on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
